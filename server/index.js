91dc8b1f9a9f478298986adb8496f4737b696a12
cizal <cizal@esignature.com.np>
2023-09-19 22:31:28 +0545

    Connected to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config;
 const cors = require("cors");
+const {mongoose} = require("mongoose");
+// const { MONGO_URL } = process.env; 
+
+
+//database connection
+mongoose
+  .connect("mongodb+srv://gautamcizal:mpp0g9sxJwxPNcWD@cluster0.pzblnty.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.log("Connection to database failed", err))
 
 const app = express();
 
